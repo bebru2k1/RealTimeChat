51f4cacc422b809375d32507316c21f125cda141
bebru2k1 <66669380+bebru2k1@users.noreply.github.com>
2021-10-01 13:35:24 +0700

    fix url socket

--- client/src/configs/socket.ts
+++ client/src/configs/socket.ts
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
-const URL = "http://localhost:5000";
+const URL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:5000"
+    : "https://chatrealtime10012021.herokuapp.com";
 
 const socket = io(URL);
 
